dfe0eb2e0cd179569bbdf506b9355e287f5a79be
victoria <viktoriya.zyk@mail.ru>
2022-10-04 22:02:13 +0300

    hw lesson 4

--- geek-cloud-common/src/main/java/ru/geekbrains/model/FileRequest.java
+++ geek-cloud-common/src/main/java/ru/geekbrains/model/FileRequest.java
@@ -5,9 +5,25 @@ import lombok.Getter;
 @Getter
 public class FileRequest implements CloudMessage {
 
+    public enum Command {
+        DELETE,
+        DOWNLOAD,
+        RENAME
+    }
+
     private final String fileName;
+    private Command command;
 
     public FileRequest(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public FileRequest(Command command, String fileName) {
+        this.command = command;
         this.fileName = fileName;
     }
 
